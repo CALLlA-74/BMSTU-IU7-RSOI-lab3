ef6b6f33cebe569eea5068bac96471f506281d28
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2023-12-11 07:57:15 +0300

    Api tests have added

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -36,7 +36,7 @@ step() {
 
   printf "=== Step %d: %s %s ===\n" "$step" "$operation" "$service"
 
-  docker compose "$operation" "$service"
+  sudo docker-compose "$operation" "$service"
   if [[ "$operation" == "start" ]]; then
     "$path"/wait-for.sh -t 120 "http://95.163.210.40:$port/manage/health" -- echo "Host 95.163.210.40:$port is active"
   fi
